dd452c539eaf658c457376ac0fc50033371f392d
vignesh-b-06 <vickybmk06@gmail.com>
2026-03-29 19:16:19 +0530

    Add files via upload

---
+++ sql task 1.sql
@@ -0,0 +1,17 @@
+-- create database schoolDb;
+-- use schoolDb;
+create table students(
+stud_id int,
+stud_name varchar(100),
+age int,
+grade varchar(100))
+
+insert into students(stud_id, stud_name, age, grade)
+values
+(1,'vicky',20,'A'),
+(2,'seth',18,'B'),
+(3,'jo',18,'B+'),
+(4,'praveen',19,'A'),
+(5,'jagan',20,'O');
+
+
